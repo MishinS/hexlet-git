97e47231278729caf85a5390187e8138cc5f1f92
sergei mishin <mishins.s@yandex.ru>
2022-03-08 23:34:02 +0300

    Change tests on the assert.equal

--- lesson_#1/tests/capitalize.test.js
+++ lesson_#1/tests/capitalize.test.js
@@ -1,11 +1,8 @@
+import { strict as assert } from 'assert';
+
 import { capitalize } from '../src/capitalize.js';
 
-if (capitalize('hello') != 'Hello') {
-	throw new Error('Function do not work');
-}
-
-if (capitalize('') != '') {
-        throw new Error('Function do not work');
-}
+assert.equal(capitalize(''), '');
+assert.equal(capitalize('hello'), 'Hello');
 
 console.log('All tests done');
